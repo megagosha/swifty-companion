8e7a33bd5e4c0cafe09c1236537c6036f4046f4d
George <gtevosov@gmail.com>
2022-05-18 16:43:10 +0300

    Initial Commit

---
+++ SwiftyCompanion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyCompanion
+//
+//  Created by George Tevosov on 18.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
